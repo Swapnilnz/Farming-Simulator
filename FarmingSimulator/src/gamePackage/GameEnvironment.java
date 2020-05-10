466a74875fc0b8eac050b40a6d941953d4851d0b
Swapnilnz <51219823+Swapnilnz@users.noreply.github.com>
2020-05-10 15:23:14 +1200

    Updated GameEnvironment class

--- FarmingSimulator/src/gamePackage/GameEnvironment.java
+++ FarmingSimulator/src/gamePackage/GameEnvironment.java
@@ -1,5 +1,7 @@
 package gamePackage;
+import java.io.IOException;
 import java.util.Scanner;
+
 
 
 public class GameEnvironment {
@@ -10,7 +12,7 @@ public class GameEnvironment {
 	private static String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	
 	
-	private void introduction() {
+	private void introduction() throws InterruptedException, IOException {
 
 		Scanner input = new Scanner(System.in);
 		System.out.println("Hello aspiring farmer! What is your name? ");
@@ -38,11 +40,11 @@ public class GameEnvironment {
 			if ((acceptableLength) && (successful)) {
 				farmerName = tempName;
 				nameFound = true;
-				System.out.println("Hello " + farmerName);
-				clearScreen();
+				System.out.println("Hello farmer " + farmerName + "!");
 			} else {
-				System.out.println("Sorry, invalid name! (Hint: name has to be 3-15 characters long and contain only letters)");
-				clearScreen();
+		        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+				System.out.println("Sorry, '" + tempName + "' is an invalid name!");
+				System.out.println("(Hint: name has to be 3-15 characters long and contain only letters)\n");
 				System.out.println("Try again:");
 				tempName = input.nextLine();
 			}
@@ -50,17 +52,28 @@ public class GameEnvironment {
 			
 		}
 	}
-	public static void clearScreen() {  
-	    System.out.flush();  
-	}  
-//	private int getNumDays() {
-//		
-//	}
+	private void inputNumDays() {
+		System.out.println("How long would you like your farming adventure to last?\nYou can choose between 5 and 15 days!");
+		Scanner input = new Scanner(System.in);
+		String tempNum = input.nextLine();
+		boolean numFound = false;
+		while (numFound == false) {
+			try {
+				int newNum = Integer.parseInt(tempNum);
+			} catch (Exception e) {
+				System.out.println("Please enter a number between 5 and 15!\nTry again:");
+				tempNum = input.nextLine();
+			}
+		}
+	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, IOException {
 		// TODO Auto-generated method stub
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+
 		GameEnvironment game = new GameEnvironment();
 		game.introduction();
+		game.inputNumDays();
 	}
 
 }
