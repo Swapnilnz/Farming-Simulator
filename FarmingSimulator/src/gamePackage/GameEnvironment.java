4571feaa96d2c113c1db87576227ef5c7f3d4227
Swapnilnz <51219823+Swapnilnz@users.noreply.github.com>
2020-05-21 21:19:04 +1200

    changed some method arguments

--- FarmingSimulator/src/gamePackage/GameEnvironment.java
+++ FarmingSimulator/src/gamePackage/GameEnvironment.java
@@ -51,58 +51,58 @@ class GameEnvironment {
 	 * View time left until crop harvest, animal happiness
 	 * @param game game
 	 */
-	void viewFarm(GameEnvironment game) {
+	void viewFarm() {
 		UI UI = new UI();
-		UI.showCrops(game.getFarm());
-		UI.showAnimals(game.getFarm());
+		UI.showCrops(this.getFarm());
+		UI.showAnimals(this.getFarm());
 	}
     /**
      * View the farm's money
      * @param game game
      */
-	void viewFarmMoney(GameEnvironment game) {
+	void viewFarmMoney() {
 		UI UI = new UI();
-		UI.showFarmMoney(game.getFarm());
+		UI.showFarmMoney(this.getFarm());
 	}
 	
 	/**
 	 * Visit Tom's Tool Market and purches various utility items
 	 * @param game game 
 	 */
-	private void visitToolMarket(GameEnvironment game) {
+	private void visitToolMarket() {
 		UI UI = new UI();
 		ToolMarket toolMarket = new ToolMarket();
 		boolean done = false;
 		while (!done) {
-			int inputNum = UI.toolMarket(game.getFarm(), toolMarket);
+			int inputNum = UI.toolMarket(this.getFarm(), toolMarket);
 			switch(inputNum) {
 				case 1:
 					// Harvester
-					toolMarket.buyHarvester(game);
+					toolMarket.buyHarvester(this);
 					break;
 				case 2:
 					// Animal Statue
-					toolMarket.buyAnimalStatue(game);
+					toolMarket.buyAnimalStatue(this);
 					break;
 				case 3:
 					// Milk Master
-					toolMarket.buyMilkMaster(game);
+					toolMarket.buyMilkMaster(this);
 					break;
 				case 4:
 					// Shear Master
-					toolMarket.buyShearMaster(game);
+					toolMarket.buyShearMaster(this);
 					break;
 				case 5:
 					// Watering Can 
-					toolMarket.buyWateringCan(game);
+					toolMarket.buyWateringCan(this);
 					break;
 				case 6:
 					// Teleportation Pad
-					toolMarket.buyTeleportationPad(game);
+					toolMarket.buyTeleportationPad(this);
 					break;
 				case 7:
 					// Animal Feed
-					toolMarket.buyAnimalFeed(game);
+					toolMarket.buyAnimalFeed(this);
 					break;
 				case 8:
 					// Exit
@@ -117,37 +117,37 @@ class GameEnvironment {
 	 * Visit crop market to buy crops
 	 * @param game game
 	 */
-	private void visitCropMarket(GameEnvironment game) {
+	private void visitCropMarket() {
 		UI UI = new UI();
 		CropMarket cropMarket = new CropMarket();
 		boolean done = false;
 		while (!done) {
-			int inputNum = UI.cropMarket(game.getFarm(), cropMarket);
+			int inputNum = UI.cropMarket(this.getFarm(), cropMarket);
 			int amount;
 			switch(inputNum) {
 				case 1:
 					// Avocado
-					amount = UI.buyAmount(game); cropMarket.buyAvocado(game, amount);
+					amount = UI.buyAmount(this); cropMarket.buyAvocado(this, amount);
 					break;
 				case 2:
 					// Corn
-					amount = UI.buyAmount(game); cropMarket.buyCorn(game, amount);
+					amount = UI.buyAmount(this); cropMarket.buyCorn(this, amount);
 					break;
 				case 3:
 					// Wheat
-					amount = UI.buyAmount(game); cropMarket.buyWheat(game, amount);
+					amount = UI.buyAmount(this); cropMarket.buyWheat(this, amount);
 					break;
 				case 4:
 					// Potato
-					amount = UI.buyAmount(game); cropMarket.buyPotato(game, amount);
+					amount = UI.buyAmount(this); cropMarket.buyPotato(this, amount);
 					break;
 				case 5:
 					// Carrot
-					amount = UI.buyAmount(game); cropMarket.buyCarrot(game, amount);
+					amount = UI.buyAmount(this); cropMarket.buyCarrot(this, amount);
 					break;
 				case 6:
 					// Apple
-					amount = UI.buyAmount(game); cropMarket.buyApple(game, amount);
+					amount = UI.buyAmount(this); cropMarket.buyApple(this, amount);
 					break;
 				case 7:
 					// Exit
@@ -162,33 +162,33 @@ class GameEnvironment {
 	 * Visit Andy's animal market to buy animals
 	 * @param game game 
 	 */
-	private void visitAnimalMarket(GameEnvironment game) {
+	private void visitAnimalMarket() {
 		UI UI = new UI();
 		AnimalMarket animalMarket = new AnimalMarket();
 		boolean done = false;
 		while (!done) {
-			int inputNum = UI.animalMarket(game.getFarm(), animalMarket);
+			int inputNum = UI.animalMarket(this.getFarm(), animalMarket);
 
 			switch(inputNum) {
 				case 1:
 					// Cow
-					int amount = UI.buyAmount(game);
-					animalMarket.buyCow(game, amount);
+					int amount = UI.buyAmount(this);
+					animalMarket.buyCow(this, amount);
 					break;
 				case 2:
 					// Pig
-					amount = UI.buyAmount(game);
-					animalMarket.buyPig(game, amount);
+					amount = UI.buyAmount(this);
+					animalMarket.buyPig(this, amount);
 					break;
 				case 3:
 					// Chicken
-					amount = UI.buyAmount(game);
-					animalMarket.buyChicken(game, amount);
+					amount = UI.buyAmount(this);
+					animalMarket.buyChicken(this, amount);
 					break;
 				case 4:
 					// Sheep
-					amount = UI.buyAmount(game);
-					animalMarket.buySheep(game, amount);
+					amount = UI.buyAmount(this);
+					animalMarket.buySheep(this, amount);
 					break;
 				case 5:
 					// exit
@@ -202,23 +202,23 @@ class GameEnvironment {
 	 * Tend to one type of crop on the farm
 	 * @param game game
 	 */
-	private void tendToCrops(GameEnvironment game) {
+	private void tendToCrops() {
 		UI UI = new UI();
 		boolean chosen = false;
-		HashMap<String, Integer> inventory = game.farm.cropInventory;
+		HashMap<String, Integer> inventory = this.farm.cropInventory;
 		while (!chosen) {
-			String crop = UI.chooseTendToCrop(game);
+			String crop = UI.chooseTendToCrop(this);
 			if (crop.equals("exit")) {
 				chosen = true;
 			} else if (inventory.get(crop) > 0) {
 
 				chosen = true;
-				for (Crop tempCrop : game.farm.cropList) {
+				for (Crop tempCrop : this.farm.cropList) {
 					String comparedCrop = tempCrop.getCropType();
 
 					if (comparedCrop.equals(crop)) {
 
-						boolean contains = game.farm.itemList.contains("Watering Can");
+						boolean contains = this.farm.itemList.contains("Watering Can");
 						int ogDays = tempCrop.getDaysTillHarvest();
 						if (contains) {
 							if (ogDays >= 4) {
@@ -248,12 +248,12 @@ class GameEnvironment {
 	 * Feed animals to make them healthier
 	 * @param game game 
 	 */
-	void feedAnimals(GameEnvironment game) {
-		if (game.farm.animalFeed > 0) {
-			for (Animal animal : game.farm.animalList) {
+	void feedAnimals() {
+		if (this.farm.animalFeed > 0) {
+			for (Animal animal : this.farm.animalList) {
 				animal.increaseHealth();
 			}
-			game.farm.animalFeed -= 1;
+			this.farm.animalFeed -= 1;
 		} else {
 			System.out.println("Sorry, you don't have any animal feed to do this!");
 		}
@@ -263,9 +263,9 @@ class GameEnvironment {
 	 * Play with animals to make them happier, or error if no animals
 	 * @param game game 
 	 */
-	void playWithAnimals(GameEnvironment game) {
-		if (game.farm.animalList.size() > 0) {
-			for (Animal animal : game.farm.animalList) {
+	void playWithAnimals() {
+		if (this.farm.animalList.size() > 0) {
+			for (Animal animal : this.farm.animalList) {
 				animal.increaseHappiness();
 			}
 		} else {
@@ -278,15 +278,15 @@ class GameEnvironment {
 	 * Adds required money
 	 * @param game game 
 	 */
-	void harvestCrops(GameEnvironment game) {
+	void harvestCrops() {
 		ArrayList<Crop> tempCropList = new ArrayList<Crop>();
-		for (Crop crop : game.farm.cropList) {
+		for (Crop crop : this.farm.cropList) {
 			if (crop.getDaysTillHarvest() == 0) {
-				game.farm.farmMoney += crop.getSellPrice();
+				this.farm.farmMoney += crop.getSellPrice();
 			} else {
 				tempCropList.add(crop);
 			}
-		game.farm.cropList = tempCropList;
+		this.farm.cropList = tempCropList;
 
 		}
 	}
@@ -295,74 +295,74 @@ class GameEnvironment {
 	 * Simple method that sets the farm's maintenance attribute to true
 	 * @param game game
 	 */
-	void tendToFarmLand(GameEnvironment game) {
-		game.farm.setMaintained(true);
+	void tendToFarmLand() {
+		this.farm.setMaintained(true);
 	}
 	
 	/**
 	 * Plays one day, with 2 or more actions
 	 * @param game game
 	 */
-	private void runDay(GameEnvironment game) {
+	private void runDay() {
 		boolean exitLoop = false;
-		int numActions = game.getNumActions();		
+		int numActions = this.getNumActions();		
 		while ((numActions >= 0) && (exitLoop != true)){
 			UI UI = new UI();
 			int chosenAction;
 			if (numActions > 0) {
-				chosenAction = UI.inputChooseAction(game, true);
+				chosenAction = UI.inputChooseAction(this, true);
 			} else {
-				chosenAction = UI.inputChooseAction(game, false);
+				chosenAction = UI.inputChooseAction(this, false);
 			}
 			switch(chosenAction) {
 				case 1:
 					// View your farm's crops and animals
-					viewFarm(game);
+					viewFarm();
 					break;
 				case 2: 
 					// View farm money (status)
-					viewFarmMoney(game);
+					viewFarmMoney();
 					break;
 				case 3:
 					// Visit tool market
-					visitToolMarket(game);
+					visitToolMarket();
 					break;
 				case 4:
 					// Visit crop market
-					visitCropMarket(game);
+					visitCropMarket();
 					break;
 				case 5:
 					// Visit animal market
-					visitAnimalMarket(game);
+					visitAnimalMarket();
 					break;
 				case 6:
 					// Move onto next day
-					game.farm.setMaintained(false);
+					this.farm.setMaintained(false);
 					exitLoop = true;
 					break;
 				case 7:
 					// Tend to crops, speed up growth, remember watering can
-					tendToCrops(game);
+					tendToCrops();
 					numActions -= 1;
 					break;
 				case 8:
 					// Feed animals to make healthier and happier
-					feedAnimals(game);
+					feedAnimals();
 					numActions -= 1;
 					break;
 				case 9:
 					// Play with animals to make happier
-					playWithAnimals(game);
+					playWithAnimals();
 					numActions -= 1;
 					break;
 				case 10:
 					// Harvest fully grown crops for cash
-					harvestCrops(game);
+					harvestCrops();
 					numActions -= 1;
 					break;
 				case 11:
 					// Tend to your farm land to keep animals happy
-					tendToFarmLand(game);
+					tendToFarmLand();
 					numActions -= 1;
 					break;
 
@@ -376,15 +376,15 @@ class GameEnvironment {
 	 * @param game game
 	 * @return int total money
 	 */
-	int getTotalMoney(GameEnvironment game) {
+	int getTotalMoney() {
 		int totalMoney = 0;
 		int happinessSum = 0;
 		int healthinessSum = 0;
-		for (Animal animal: game.farm.animalList) {
+		for (Animal animal: this.farm.animalList) {
 			happinessSum += animal.getHappiness();
 			healthinessSum += animal.getHealth();
 		}
-		int aLSize = game.farm.animalList.size();
+		int aLSize = this.farm.animalList.size();
 		if (aLSize > 0) {
 			int happinessAv = Math.round(happinessSum / aLSize);
 			int healthinessAv = Math.round(healthinessSum / aLSize);
@@ -397,44 +397,44 @@ class GameEnvironment {
 	 * Does end of day calculations to add money, harvest if necessary etc
 	 * @param game game
 	 */
-	void endDay(GameEnvironment game) {
+	void endDay() {
 		// Add money (Milk Master; Shear Master; Harvester) TO DO: Normal
-		if (game.farm.itemList.contains("Milk Master")) {
-			for (Animal animal: game.farm.animalList) {
+		if (this.farm.itemList.contains("Milk Master")) {
+			for (Animal animal: this.farm.animalList) {
 				if (animal instanceof Cow) {
-					((Cow) animal).milk(game); // Adds milking money
+					((Cow) animal).milk(this); // Adds milking money
 				}
 			}
 		}
-		if (game.farm.itemList.contains("Shear Master")) {
-			for (Animal animal: game.farm.animalList) {
+		if (this.farm.itemList.contains("Shear Master")) {
+			for (Animal animal: this.farm.animalList) {
 				if (animal instanceof Sheep) {
-					((Sheep) animal).shear(game); // Adds shearing money
+					((Sheep) animal).shear(this); // Adds shearing money
 				}
 			}
 		}
-		if (game.farm.itemList.contains("Harvester")) {
-			if (game.farm.cropList.size() > 0) {
+		if (this.farm.itemList.contains("Harvester")) {
+			if (this.farm.cropList.size() > 0) {
 				Random rnd = new Random();
-				int rand = rnd.nextInt(game.farm.cropList.size());
-				game.farm.cropList.get(rand).harvest(game); // Adds harvester money
+				int rand = rnd.nextInt(this.farm.cropList.size());
+				this.farm.cropList.get(rand).harvest(this); // Adds harvester money
 			}
 		}
 		// Adds animal money
-		int moneyToAdd = getTotalMoney(game);
-		game.farm.farmMoney += moneyToAdd;
+		int moneyToAdd = getTotalMoney();
+		this.farm.farmMoney += moneyToAdd;
 		// Decrease all days till harvest by 1 
-		for (Crop crop : game.farm.cropList) {
-			if (crop.getDaysTillHarvest() > game.farm.getGrowthRate()) {
-				crop.daysTillHarvest -= game.farm.getGrowthRate();
+		for (Crop crop : this.farm.cropList) {
+			if (crop.getDaysTillHarvest() > this.farm.getGrowthRate()) {
+				crop.daysTillHarvest -= this.farm.getGrowthRate();
 			}
 		}
 		// Decrease animal happiness if farm is not maintained and doesn't have animal statue
-		boolean containsStatue = game.farm.itemList.contains("Animal Statue");
-		boolean maintained = game.farm.isMaintained();
+		boolean containsStatue = this.farm.itemList.contains("Animal Statue");
+		boolean maintained = this.farm.isMaintained();
 		if ((!containsStatue && !maintained)) {
-			for (int i=0; i < game.farm.animalList.size(); i++) {
-				game.farm.animalList.get(i).decreaseHappiness();
+			for (int i=0; i < this.farm.animalList.size(); i++) {
+				this.farm.animalList.get(i).decreaseHappiness();
 				
 			}
 		}
@@ -449,20 +449,20 @@ class GameEnvironment {
 	 * @param game game
 	 * @return score game score
 	 */
-	int calculateScore(GameEnvironment game) {
-		int money = game.farm.getFarmMoney();
-		int duration = game.gameDuration;
+	int calculateScore() {
+		int money = this.farm.getFarmMoney();
+		int duration = this.gameDuration;
 		int score = 0;
 		
 		int happinessSum = 0;
 		int healthinessSum = 0;
 		int happinessAv = 0;
 		int healthinessAv = 0;
-		for (Animal animal: game.farm.animalList) {
+		for (Animal animal: this.farm.animalList) {
 			happinessSum += animal.getHappiness();
 			healthinessSum += animal.getHealth();
 		}
-		int aLSize = game.farm.animalList.size();
+		int aLSize = this.farm.animalList.size();
 		if (aLSize > 0) {
 			happinessAv = Math.round(happinessSum / aLSize);
 			healthinessAv = Math.round(healthinessSum / aLSize);
@@ -477,11 +477,11 @@ class GameEnvironment {
 	 * End the adventure as days have run out
 	 * @param game game
 	 */
-	private void endAdventure(GameEnvironment game) {
+	private void endAdventure() {
 		// Display farm's name, game's duration, profit
 		UI UI = new UI();
-		UI.displayEndAdventure(game);
-		int score = calculateScore(game);
+		UI.displayEndAdventure(this);
+		int score = calculateScore();
 		// Print Score
 		System.out.println("------------------------------------------");
 		System.out.println("Your score was");
@@ -495,15 +495,15 @@ class GameEnvironment {
 	 * Start the adventure as set up is done
 	 * @param game game
 	 */
-	private void startAdventure(GameEnvironment game) {
+	private void startAdventure() {
 		UI startInput = new UI();
-		startInput.inputStartAdventure(game);
-		while (game.getNumDays() > 0) {
-			runDay(game);
+		startInput.inputStartAdventure(this);
+		while (this.getNumDays() > 0) {
+			runDay();
 			// Happens at the end of the day
-			endDay(game);
-			game.numDays--;
-			game.gameDuration++;
+			endDay();
+			this.numDays--;
+			this.gameDuration++;
 		}
 	}
 	
@@ -511,10 +511,10 @@ class GameEnvironment {
 	 * Get farmer name and age
 	 * @param game game
 	 */
-	private void getFarmer(GameEnvironment game) {
+	private void getFarmerStats() {
 		UI UI = new UI();
-		String farmerName = UI.inputFarmerName(game);
-		int farmerAge = UI.inputFarmerAge(game);
+		String farmerName = UI.inputFarmerName(this);
+		int farmerAge = UI.inputFarmerAge(this);
 		farmer = new Farmer(farmerName, farmerAge);
 	}
 	
@@ -547,6 +547,19 @@ class GameEnvironment {
 	}
 	
 	/**
+	 * Lunch main screen
+	 * @param game game
+	 */
+	public void launchMainsScreen() {
+		MainScreen mainWindow = new MainScreen(this);
+	}
+	
+	public void closeMainScreen(MainScreen mainWindow) {
+		mainWindow.closeWindow();
+	}
+	
+	
+	/**
 	 * Main method; used to start and end game
 	 * @param args no args needed
 	 */
@@ -554,12 +567,12 @@ class GameEnvironment {
 
 		GameEnvironment game = new GameEnvironment();
 		UI UI = new UI();
-		game.getFarmer(game);
+		game.getFarmerStats();
 		UI.inputNumDays(game);
 		UI.inputChooseFarm(game);
 		UI.inputFarmName(game);
-		game.startAdventure(game);
-		game.endAdventure(game);
+		game.startAdventure();
+		game.endAdventure();
 
 	}
 
